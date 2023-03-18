b182e66f107966957f95013d0d8f21b09f741a1a
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-18 13:10:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return window.customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+  }
+  
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+}
+
+const LeastFavoriteCustomer = "Maybe";
+
+function changeLeastFavoriteCustomer() {
+    LeastFavoriteCustomer = "Nop";
+    return LeastFavoriteCustomer;
+
+}
